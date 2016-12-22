d1fe502ca9f983e4416fbb02a6ae64ec83454343
moon <moon@eazler.com>
2016-12-23 00:59:22 +0900

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+
+module.exports = function(grunt) {
+
+    var config = {};
+
+    config.clean = {
+        build : {
+            files : [{
+                dot : true,
+                src : [
+                    "css/main.css",
+                    "css/404.css"
+                ]
+            }]
+        }
+    };
+
+    config.less = {
+        development: {
+            files: {
+                "css/main.css" : "less/__main.less",
+                "css/404.css" : "less/__404.less"
+            }
+        }
+    };
+
+    grunt.initConfig(config);
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-less");
+};
+
